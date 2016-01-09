3de3191a52169e44d69373750d31b5dbc8fc9c03
yuni-net <yuni.net.liberty@gmail.com>
2016-01-09 16:14:34 +0900

    Synchronizer implemented alot

--- src/Synchronizer.cpp
+++ src/Synchronizer.cpp
@@ -1,6 +1,179 @@
+#include <fstream>
 #include "Synchronizer.h"
 
-namespace Synchronizer
+namespace OnlineParty
 {
+	Synchronizer::Synchronizer()
+	{
+		state = State::waiting_reply_join;
+		load_config();
+		send_join_request();
+	}
+
+	void Synchronizer::update()
+	{
+		if (p2p.are_there_any_left_datas() == false)
+		{
+			return;
+		}
+
+		if (state == State::waiting_reply_join)
+		{
+			wait_reply_join();
+		}
+		else
+		{
+			process_request();
+		}
+	}
+
+
+	/**
+	@brief I load the config to server_surfer.
+	@detail If I failed to load it,  I set the default parameter.
+	*/
+	void Synchronizer::load_config()
+	{
+		std::ifstream ifs("data/config/Synchronizer.json", std::ios::in);
+		picojson::value value;
+		std::string result = picojson::parse(value, ifs);
+		if (result.empty() == false)
+		{
+			// I failed to load config.
+			// I set the default parameter.
+			fw::IP server_ip;
+			server_ip.set_by_hostname("192.168.11.1");
+			server_surfer.set(server_ip, 9696);
+			return;
+		}
+
+		picojson::object & root = value.get<picojson::object>();
+		const std::string & server_hostname = root["server_ip"].get<std::string>();
+		const unsigned short server_port = static_cast<unsigned short>(root["server_port"].get<double>());
+
+		fw::IP server_ip;
+		server_ip.set_by_hostname(server_hostname);
+		server_surfer.set(server_ip, server_port);
+	}
+
+	/**
+	@brief I send the join request.
+	*/
+	void Synchronizer::send_join_request()
+	{
+		fw::Bindata join_request;
+		join_request.add(std::string("{"));
+		join_request.add(std::string("\"signature\": \"OnlineParty\", "));
+		join_request.add(std::string("\"version\": 0, "));
+		join_request.add(std::string("\"request\": \"join\", "));
+		join_request.add(std::string("}"));
+		p2p.send(server_surfer, join_request);
+	}
+
+	/**
+	@brief I expected to receive the reply for join.
+	*/
+	void Synchronizer::wait_reply_join()
+	{
+		while (p2p.are_there_any_left_datas())
+		{
+			picojson::value value;
+			if (get_reply_server(value) == false)
+			{
+				continue;
+			}
+
+			// The program will be terminated when the data is invalid
+			// Cuz the keys have not found.
+			// "the keys": exp. "signature", "version" and so on.
+			picojson::object & root = value.get<picojson::object>();
+			if (root["sigunature"].get<std::string>() != "OnlineParty")
+			{
+				continue;
+			}
+			if (static_cast<int>(root["version"].get<double>()) != 0)
+			{
+				continue;
+			}
+
+			if (root["reply"].get<std::string>() == "fully occupied")
+			{
+				// There are no vacant table.
+			}
+			else if (root["reply"].get<std::string>() == "join")
+			{
+				throw_all_left_datas_to_trash();
+				save_my_ID(root);
+				save_others_surfer(root);
+				state = State::waiting_request_sync;
+			}
+		}
+	}
+
+	/**
+	@brief I process the requests for synchronization.
+	*/
+	void Synchronizer::process_request()
+	{
+		// todo
+	}
+
+	/**
+	@brief I pop the received data, parse it and set the result to "value".
+	@param Set the "picojson::value" object for receive parsed data.
+	@return
+		true: success
+		false: failure
+	*/
+	bool Synchronizer::get_reply_server(picojson::value & value)
+	{
+		fw::Bindata data;
+		fw::NetSurfer surfer;
+		if (p2p.pop_received_data(data, surfer) == false)
+		{
+			return false;
+		}
+		if (surfer != server_surfer)
+		{
+			return false;
+		}
+
+		const std::string & result = picojson::parse(value, data.buffer());
+		if (result.empty() == false)
+		{
+			return false;
+		}
+
+		return true;
+	}
+
+	void Synchronizer::throw_all_left_datas_to_trash()
+	{
+		fw::Bindata data;
+		fw::NetSurfer surfer;
+		while (p2p.are_there_any_left_datas())
+		{
+			p2p.pop_received_data(data, surfer);
+		}
+	}
+
+	void Synchronizer::save_my_ID(picojson::object & root)
+	{
+		my_ID = static_cast<int>(root["your ID"].get<double>());
+	}
+
+	void Synchronizer::save_others_surfer(picojson::object & root)
+	{
+		picojson::array & others = root["the others"].get<picojson::array>();
+		for (auto rator = others.begin(); rator != others.end(); ++rator)
+		{
+			picojson::object other = rator->get<picojson::object>();
+			// todo
+			other["ID"].get<double>();
+			other["IP"].get<std::string>();
+			other["port"].get<double>();
+		}
+	}
+
 
 }
--- src/Synchronizer.h
+++ src/Synchronizer.h
@@ -1,10 +1,50 @@
 #pragma once
 
+#include "picojson.h"
+#include <simplect3D.h>
+
 namespace OnlineParty
 {
+	/**
+	This manage all of network communication on this game.
+	*/
 	class Synchronizer
 	{
 	public:
+		Synchronizer();
+		void update();
 
+
+
+	private:
+		struct State
+		{
+			enum
+			{
+				waiting_reply_join,
+				waiting_request_sync
+			};
+		};
+
+		struct MemberP2P
+		{
+			int ID;
+			fw::NetSurfer surfer;
+			unsigned long long last_sync;
+		};
+
+		fw::P2P p2p;
+		fw::NetSurfer server_surfer;
+		int state;
+		int my_ID;
+
+		void load_config();
+		void send_join_request();
+		void wait_reply_join();
+		void process_request();
+		bool get_reply_server(picojson::value & value);
+		void throw_all_left_datas_to_trash();
+		void save_my_ID(picojson::object & root);
+		void save_others_surfer(picojson::object & root);
 	};
 }
