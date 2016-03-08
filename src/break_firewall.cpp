1a9e8f94b6b7006b4e57a25820bf76ca58cf55aa
yuni-net <yuni.net.liberty@gmail.com>
2016-03-08 11:24:58 +0900

    send to myself and recvfrom

--- src/break_firewall.cpp
+++ src/break_firewall.cpp
@@ -11,30 +11,30 @@ namespace OnlineParty
 		recv_addr.sin_addr.S_un.S_addr = INADDR_ANY;
 
 		unsigned short port = 9696;
-		do
+		while (true)
 		{
 			recv_addr.sin_port = htons(port);
 			int result = bind(sock, reinterpret_cast<sockaddr *>(&recv_addr), sizeof(recv_addr));
-			if (result == -1)
-			{
-				++port;
-				continue;
-			}
-			else
+			if (result != -1)
 			{
 				break;
 			}
-		} while (true);
+			++port;
+		}
 
-		fd_set fds;
-		FD_ZERO(&fds);
-		FD_SET(sock, &fds);
+		sockaddr_in sendaddr;
+		memset(&sendaddr, 0, sizeof(sendaddr));
+		sendaddr.sin_family = AF_INET;
+		sendaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+		sendaddr.sin_port = htons(port);
+		char damy = 0;
+		sendto(sock, &damy, sizeof(damy), 0, (sockaddr *) &sendaddr, sizeof(sendaddr));
 
-		timeval timev;
-		timev.tv_sec = 0;
-		timev.tv_usec = 8;
-
-		select(0, &fds, NULL, NULL, &timev);
+		const int bufsize = 256;
+		char buffer [bufsize];
+		sockaddr_in ano_addr;
+		int addr_len = sizeof(sockaddr_in);
+		recvfrom(sock, buffer, bufsize, 0, reinterpret_cast<sockaddr *>(&ano_addr), &addr_len);
 
 		closesocket(sock);
 	}
