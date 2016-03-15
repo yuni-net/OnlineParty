14728366c228561fffa55cb7298bd5f0cd8f4db2
yuni-net <yuni.net.liberty@gmail.com>
2016-03-15 17:56:59 +0900

    I will break firewall!!

--- src/break_firewall.cpp
+++ src/break_firewall.cpp
@@ -1,39 +1,19 @@
 #include <simplect3D.h>
+#include "FirewallInstallHelper.h"
 
 namespace OnlineParty
 {
-	void break_firewall_actually(void * parameter)
-	{
-		SOCKET sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-		sockaddr_in recv_addr;
-		recv_addr.sin_family = AF_INET;
-		recv_addr.sin_addr.S_un.S_addr = INADDR_ANY;
-
-		unsigned short port = 9697;
-		while (true)
-		{
-			recv_addr.sin_port = htons(port);
-			int result = bind(sock, reinterpret_cast<sockaddr *>(&recv_addr), sizeof(recv_addr));
-			if (result != -1)
-			{
-				break;
-			}
-			++port;
-		}
-
-		const int bufsize = 256;
-		char buffer[bufsize];
-		sockaddr_in ano_addr;
-		int addr_len = sizeof(sockaddr_in);
-		recvfrom(sock, buffer, bufsize, 0, reinterpret_cast<sockaddr *>(&ano_addr), &addr_len);
-
-		closesocket(sock);
-	}
-
 	void break_firewall()
 	{
-		fw::NetWork::init_ifneed();
-		fw::newthread(break_firewall_actually, nullptr);
-	}
+		static const int bufsize = 1024;
+		char mypath[bufsize];
+		GetModuleFileName(NULL, mypath, bufsize);
+		const bool registered = CanLaunchMultiplayerGameA(mypath) != 0;
+		if (registered)
+		{
+			return;
+		}
 
+		system(fw::cnct()<<".\\data\\FirewallBreaker.exe \""<<mypath<"\" -friend OnlineParty_debug");
+	}
 }
